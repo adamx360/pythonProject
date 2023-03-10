5b5c0557fa1ada92efddaa9f173600e2f0ffb96c
adamx360 <sliwax360@gmail.com>
2023-03-10 20:38:19 +0100

    iuh

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import webbrowser
+import requests
 
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+pageurl = input("podaj adres strony: ")
+date = input("podaj date: ")
+url = "http://archive.org/wayback/available?url=" + pageurl + "&timestamp=" + str(date)
+response = requests.get(url)
+d = response.json()
+print(d)
+page = d["archived_snapshots"]["closest"]["url"]
+webbrowser.open(page)
